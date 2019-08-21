da54c766c64838c4becf1bff8c9a1896becf7d38
hlyongjiu <hlyongjiu@163.com>
2019-08-21 16:00:50 +0800

    vue test

---
+++ src/components/day01/HelloVue.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="vue">
+        Hello Vue.js!{{message}}
+    </div>
+</template>
+    <script type="text/javascript">
+        export default{
+            name:"HelloVue",
+            data(){
+                return{
+                    message : "啦啦啦啦"
+                }
+            }
+        }
+    </script>
+    <style type="text/css">
+        #vue{
+            color:green;
+            font-size:28px;
+        }
+    </style>
+
